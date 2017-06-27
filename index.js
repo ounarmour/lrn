1d3c89eb99f80bf3f38a198fe0e391ed9d15d3a1
Louis Narmour <lou@narmour.com>
2017-06-27 12:18:55 -0700

    commit 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.printMsg = function() {
-	console.log("This would be a really useful math function");
+	console.log("This would be a really useful math function.\n");
+	console.log("Hello from v1.0.1\n");
 }
